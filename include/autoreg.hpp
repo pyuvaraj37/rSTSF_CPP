17d4036183a699d0edfd4399e069b08ae2dd5617
c-jpeg <chloejane.cuevas@gmail.com>
2026-01-04 16:36:49 -0800

    Back in business

--- include/autoreg.hpp
+++ include/autoreg.hpp
@@ -5,23 +5,8 @@
 #include <cmath>
 using namespace std; 
 
-/*
-Auto Regressive Function 
-AR(p)   p = order 
-y(t) = c + phi(1)*y(t-1) + ...+ phi(p)*y(t-p) + E
-c = constant = intercent 
-phi = lagged values 
-E = error terms 
-*/
-
 //Function Declaration 
 vector<double> BurgAlgorithm(const vector<double>& data, int order);
 vector<vector<double>> ar_coeffs(const vector<vector<double>> &X);
-
-
-
-
-
-
 
 #endif 
